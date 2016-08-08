edfe346c044c0833222500b66db683d27837aa87
Dan Leeper <dan.leeper@gmail.com>
2016-08-08 15:23:41 -0400

    Step 8.9: Limit detail view to logged in user

--- client/imports/parties/parties-form.component.ts
+++ client/imports/parties/parties-form.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Meteor } from 'meteor/meteor';
 import { Parties } from '../../../both/collections/parties/parties.collection';
 
- 
+
 import template from './parties-form.component.html';
- 
+
 @Component({
-  selector: 'parties-form',
-  template,
-  directives: [REACTIVE_FORM_DIRECTIVES]
+    selector: 'parties-form',
+    template,
+    directives: [REACTIVE_FORM_DIRECTIVES]
 })
-export class PartiesFormComponent { 
+export class PartiesFormComponent {
     addForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this.addForm = this.formBuilder.group({
@@ -28,13 +29,17 @@ export class PartiesFormComponent {
         this.addForm.controls['description']['updateValue']('');
         this.addForm.controls['location']['updateValue']('');
     }
- 
+
     addParty() {
         if (this.addForm.valid) {
-            Parties.insert(this.addForm.value);
- 
-          // XXX will be replaced by this.addForm.reset() in RC5+
-         this.resetForm();
+            if (Meteor.userId()) {
+                Parties.insert(Object.assign({}, this.addForm.value, { owner: Meteor.userId() }));
+
+                // XXX will be replaced by this.addForm.reset() in RC5+
+                this.resetForm();
+            } else {
+                alert('Please log in to add a party');
+            }
+        }
     }
-  }
 }
