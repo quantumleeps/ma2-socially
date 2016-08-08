ac7279c6c1c89367a6c51aebf8882a4d52f99778
Dan Leeper <dan.leeper@gmail.com>
2016-08-08 11:01:46 -0400

    Step 5.18: Create and make route for Party Details

---
+++ client/imports/parties/party-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, NgZone, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Tracker } from 'meteor/tracker';
+
+import { Parties } from '../../../both/collections/parties/parties.collection';
+
+ 
+import template from './party-details.component.html';
+ 
+@Component({
+  selector: 'party-details',
+  template
+})
+export class PartyDetailsComponent implements OnInit {
+    partyId: string;
+    party: any;
+
+    constructor(private route: ActivatedRoute, private ngZone: NgZone) {}
+    ngOnInit () {
+        this.route.params
+            .map(params => params['partyId'])
+            .subscribe(partyId => {
+                this.partyId = partyId;
+                Tracker.autorun(() => {
+                    this.ngZone.run(() => {
+                        this.party = Parties.findOne(this.partyId);
+                    })
+                });
+            });
+    }
+}
